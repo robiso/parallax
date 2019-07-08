ba7928f43b278e3d95c468d431c42cef2ace8db0
StephanStanisic <stephanstanisic@gmail.com>
2019-07-08 23:30:22 +0200

    Added sample database

--- readme.md
+++ readme.md
@@ -4,3 +4,5 @@ _for WonderCMS 2.x.x_
 This is just a test of mine. Use at your own risk. Works best with a floating summernote editor.
 
 Demo: https://www.wonder.stanisic.nl
+
+The sample-database.js contains the same HTML from the demo website. Uses /loginURL as login url and 'admin' as password.
---
+++ sample-database.js
@@ -0,0 +1,96 @@
+{
+    "config": {
+        "dbVersion": "2.7.0",
+        "siteTitle": "Parallax theme",
+        "theme": "dev",
+        "defaultPage": "home",
+        "login": "login",
+        "password": "$2y$10$yIojAr.LVbCzGdJK0XSyUe9i3f\/pwXi33a8qnUq4u63tIotGaJ.R.",
+        "menuItems": {
+            "0": {
+                "name": "Home",
+                "slug": "home",
+                "visibility": "show"
+            },
+            "1": {
+                "name": "Example",
+                "slug": "example",
+                "visibility": "show"
+            }
+        },
+        "themeHeaderHeight": "100vh"
+    },
+    "pages": {
+        "404": {
+            "title": "404",
+            "keywords": "404",
+            "description": "404",
+            "content": "<h1>Sorry, we couldn't find that page.<\/h1><p>&nbsp;<\/p><p><a href=\"\/\"><button>Go home<\/button><\/a><\/p>"
+        },
+        "home": {
+            "title": "Home",
+            "keywords": "Keywords, are, good, for, search, engines",
+            "description": "Welcome to sample!<br>\nThis is an simple example of this template. <br>\nIt is awesome! Use it now for your site!",
+            "content": "<h1 style=\"text-align: center;\">\n<svg xmlns=\"http:\/\/www.w3.org\/2000\/svg\" viewBox=\"0 0 576 512\" style=\"\n    height: 1em;\n    fill: #636363;\n\"><path d=\"M463.7 0H112.3c-4.2 0-8.1 2.2-10.3 5.8L1.7 168.6c-2.7 4.4-2.2 10.1 1.2 14l276 325.2c4.8 5.6 13.4 5.6 18.2 0l276-325.2c3.4-3.9 3.8-9.6 1.2-14L474 5.8c-2.2-3.6-6.1-5.8-10.3-5.8zm-13.6 36l74.3 124h-83L384.6 36h65.5zM345 36l56.8 124H174.1L231 36h114zm-219.1 0h65.5l-56.8 124h-83l74.3-124zM61.2 192h73L216 384 61.2 192zm112 0h229.5L288 455.8 173.2 192zM360 384l81.8-192h73L360 384z\"><\/path><\/svg><\/h1><h1 style=\"text-align: center;\">This is a simple theme.<\/h1><p style=\"text-align: center;\">&nbsp;<\/p><h3 style=\"text-align: center;\"><font color=\"#636363\">Parallax theme for WonderCMS<\/font><\/h3>\n\n<h4><br><\/h4><p><br><\/p><p><br><\/p><p><\/p><hr><p><\/p><p><br><\/p><p><br><\/p><p><br><\/p><p>This is my attempt of making a WonderCMS theme. I have never tried&nbsp;<span style=\"background-color: transparent; color: inherit;\">making a&nbsp;<\/span><span style=\"background-color: transparent; color: inherit;\">theme&nbsp;<\/span><span style=\"background-color: transparent; color: inherit;\">myself, so I am pretty happy with this result.&nbsp; Here are a few tips on how to use this theme.<\/span><\/p><p><span style=\"background-color: transparent; color: inherit;\">&nbsp;<\/span><\/p><p><span style=\"background-color: transparent; color: inherit;\"><br><\/span><\/p><div class=\"row\"><div class=\"col-sm-4\" style=\"\n    max-width: 400px;\n    margin: 0 auto;\n\"><svg xmlns=\"http:\/\/www.w3.org\/2000\/svg\" viewBox=\"0 0 512 512\" style=\"\n    height: 5em;\n\"><path d=\"M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm16 336c0 8.822-7.178 16-16 16H48c-8.822 0-16-7.178-16-16V112c0-8.822 7.178-16 16-16h416c8.822 0 16 7.178 16 16v288zM112 232c30.928 0 56-25.072 56-56s-25.072-56-56-56-56 25.072-56 56 25.072 56 56 56zm0-80c13.234 0 24 10.766 24 24s-10.766 24-24 24-24-10.766-24-24 10.766-24 24-24zm207.029 23.029L224 270.059l-31.029-31.029c-9.373-9.373-24.569-9.373-33.941 0l-88 88A23.998 23.998 0 0 0 64 344v28c0 6.627 5.373 12 12 12h360c6.627 0 12-5.373 12-12v-92c0-6.365-2.529-12.47-7.029-16.971l-88-88c-9.373-9.372-24.569-9.372-33.942 0zM416 352H96v-4.686l80-80 48 48 112-112 80 80V352z\"><\/path><\/svg><p><b style=\"background-color: transparent; color: inherit;\"><br><\/b><\/p><p><b style=\"background-color: transparent; color: inherit;\">Use small blurred header images<\/b><br><\/p><p>Take a look at the header image I used on the <a href=\"https:\/\/wonder.stanisic.nl\/example\">example page<\/a>, it is way to small for the screen. But because&nbsp;<span style=\"background-color: transparent; color: inherit;\">it is already blurred, the blurry scaling doesn't show that much.&nbsp;<\/span><\/p><p><br><\/p><p><br><\/p><\/div><div class=\"col-sm-4\" style=\"\n    max-width: 400px;\n    margin: 0 auto;\n\">\n<svg xmlns=\"http:\/\/www.w3.org\/2000\/svg\" viewBox=\"0 0 384 512\" style=\"\n    height: 5em;\n\"><path d=\"M369.9 97.9L286 14C277 5 264.8-.1 252.1-.1H48C21.5 0 0 21.5 0 48v416c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48V131.9c0-12.7-5.1-25-14.1-34zm-22.6 22.7c2.1 2.1 3.5 4.6 4.2 7.4H256V32.5c2.8.7 5.3 2.1 7.4 4.2l83.9 83.9zM336 480H48c-8.8 0-16-7.2-16-16V48c0-8.8 7.2-16 16-16h176v104c0 13.3 10.7 24 24 24h104v304c0 8.8-7.2 16-16 16zm-48-244v8c0 6.6-5.4 12-12 12H108c-6.6 0-12-5.4-12-12v-8c0-6.6 5.4-12 12-12h168c6.6 0 12 5.4 12 12zm0 64v8c0 6.6-5.4 12-12 12H108c-6.6 0-12-5.4-12-12v-8c0-6.6 5.4-12 12-12h168c6.6 0 12 5.4 12 12zm0 64v8c0 6.6-5.4 12-12 12H108c-6.6 0-12-5.4-12-12v-8c0-6.6 5.4-12 12-12h168c6.6 0 12 5.4 12 12z\" style=\"\n    \/* height: 4em; *\/\n\"><\/path><\/svg><p><b><br><\/b><\/p><p><b>Page background images<\/b><\/p><p>Upload a picture with the name of the page to the files section. For this page, I've uploaded home.jpg to the files selection. This image is then used as the parallax background.<\/p><p><br><\/p><p><br><\/p><\/div><div class=\"col-sm-4\" style=\"\n    max-width: 400px;\n    margin: 0 auto;\n\">\n<svg xmlns=\"http:\/\/www.w3.org\/2000\/svg\" viewBox=\"0 0 512 512\" style=\"\n    height: 5em;\n\"><path d=\"M416 64v384h56a8 8 0 0 1 8 8v16a8 8 0 0 1-8 8H328a8 8 0 0 1-8-8v-16a8 8 0 0 1 8-8h56V272H128v176h56a8 8 0 0 1 8 8v16a8 8 0 0 1-8 8H40a8 8 0 0 1-8-8v-16a8 8 0 0 1 8-8h56V64H40a8 8 0 0 1-8-8V40a8 8 0 0 1 8-8h144a8 8 0 0 1 8 8v16a8 8 0 0 1-8 8h-56v176h256V64h-56a8 8 0 0 1-8-8V40a8 8 0 0 1 8-8h144a8 8 0 0 1 8 8v16a8 8 0 0 1-8 8z\"><\/path><\/svg><p><b><br><\/b><\/p><p><b>Edit the header and subtitle<\/b><\/p><p>You can edit the header and subtitle just by clicking on them. This works the best with the summernote-floating plugin. You can edit some more things in an extra tab in settings.<\/p><p><br><\/p><p><span style=\"background-color: transparent; color: inherit;\"><br><\/span><\/p><p><span style=\"background-color: transparent; color: inherit;\">&nbsp;<\/span><br><\/p><\/div><\/div><p><\/p><hr><p><\/p><p><span style=\"background-color: transparent; color: inherit;\"><br><\/span><\/p><p><span style=\"background-color: transparent; color: inherit;\"><br><\/span><\/p><h2><br>CC-0<\/h2><p><br><\/p><p>Do what you want with this theme. If you make improvements, please send me an email so I can improve this theme:<\/p><p><br><\/p><h4>stephan\n<svg style=\"height: 0.8em;\" xmlns=\"http:\/\/www.w3.org\/2000\/svg\" viewBox=\"0 0 512 512\"><path d=\"M256 8C118.941 8 8 118.919 8 256c0 137.058 110.919 248 248 248 52.925 0 104.68-17.078 147.092-48.319 5.501-4.052 6.423-11.924 2.095-17.211l-5.074-6.198c-4.018-4.909-11.193-5.883-16.307-2.129C346.93 457.208 301.974 472 256 472c-119.373 0-216-96.607-216-216 0-119.375 96.607-216 216-216 118.445 0 216 80.024 216 200 0 72.873-52.819 108.241-116.065 108.241-19.734 0-23.695-10.816-19.503-33.868l32.07-164.071c1.449-7.411-4.226-14.302-11.777-14.302h-12.421a12 12 0 0 0-11.781 9.718c-2.294 11.846-2.86 13.464-3.861 25.647-11.729-27.078-38.639-43.023-73.375-43.023-68.044 0-133.176 62.95-133.176 157.027 0 61.587 33.915 98.354 90.723 98.354 39.729 0 70.601-24.278 86.633-46.982-1.211 27.786 17.455 42.213 45.975 42.213C453.089 378.954 504 321.729 504 240 504 103.814 393.863 8 256 8zm-37.92 342.627c-36.681 0-58.58-25.108-58.58-67.166 0-74.69 50.765-121.545 97.217-121.545 38.857 0 58.102 27.79 58.102 65.735 0 58.133-38.369 122.976-96.739 122.976z\"><\/path><\/svg> stanisic.nl<\/h4>\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n",
+            "addition_content_1": "<p>This is an second text block.<\/p><p><br><\/p><p><br><\/p><p>Some line breaks.<\/p>",
+            "addition_content_show_1": "show",
+            "kaas": "kaaskop",
+            "heading": "Parallax",
+            "subtitle": "This is an awesome parallax<br>\ntheme for WonderCMS!",
+            "themeHeaderHeight": "100",
+            "customSetting": "Default value"
+        },
+        "example": {
+            "title": "Example",
+            "keywords": "Keywords, are, good, for, search, engines",
+            "description": "Example page!<br>\nThis is an awesome theme!",
+            "content": "<h1>Editable header height<\/h1><p><br><\/p><p>This page has a smaller header. You can change the height<\/p><p>of each header inside the settings of each page.<\/p><p><br><\/p><p><\/p><hr><p><\/p><p><br><\/p><p><img style=\"width: 750px;\" src=\"https:\/\/www.wonder.stanisic.nl\/files\/Screenshot%20from%202019-07-07%2022.27.04.png\"><\/p>",
+            "themeHeaderHeight": "70",
+            "subtitle": "Example page<br>\nWith some extra stuff"
+        }
+    },
+    "blocks": {
+        "subside": {
+            "content": "<h3>Nice footer block.<\/h3><p>Y'know, for social icons and stuff.&nbsp;<\/p><p><br><\/p><div class=\"row\"><div class=\"col-sm-6 col-md-4 col-lg-3 col-md-offset-2 col-lg-offset-3\"><h4>Company name, ltd.<\/h4><p>Some street 1337<br><span style=\"background-color: transparent; color: inherit;\">Some City, SA<\/span><br>\nSome Country<\/p><p>Mon-Fri, 9 to 5<\/p><p><br><\/p><\/div><div class=\"col-sm-6 col-md-4 col-lg-3\"><h4>How you can find us<\/h4>\n<p>\n<svg xmlns=\"http:\/\/www.w3.org\/2000\/svg\" viewBox=\"0 0 512 512\" style=\"\n    fill: #fff;\n    height: 1.5em;\n    vertical-align: middle;\n    margin-top: -0.1em;\n\"><path d=\"M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z\"><\/path><\/svg> &nbsp; \/ <a href=\"http:\/\/#\" target=\"_blank\">example<\/a><\/p>\n<p>\n<svg xmlns=\"http:\/\/www.w3.org\/2000\/svg\" viewBox=\"0 0 512 512\" style=\"\n    fill: #fff;\n    height: 1.5em;\n    vertical-align: middle;\n    margin-top: -0.1em;\n\">\n<path d=\"M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z\"><\/path><\/svg> &nbsp; \/ <a href=\"http:\/\/#\" target=\"_blank\">example<\/a><\/p><p>\n<svg xmlns=\"http:\/\/www.w3.org\/2000\/svg\" viewBox=\"0 0 512 512\" style=\"\n    fill: #fff;\n    height: 1.5em;\n    vertical-align: middle;\n    margin-top: -0.1em;\n\"><path d=\"M71.5 142.3c.6-5.9-1.7-11.8-6.1-15.8L20.3 72.1V64h140.2l108.4 237.7L364.2 64h133.7v8.1l-38.6 37c-3.3 2.5-5 6.7-4.3 10.8v272c-.7 4.1 1 8.3 4.3 10.8l37.7 37v8.1H307.3v-8.1l39.1-37.9c3.8-3.8 3.8-5 3.8-10.8V171.2L241.5 447.1h-14.7L100.4 171.2v184.9c-1.1 7.8 1.5 15.6 7 21.2l50.8 61.6v8.1h-144v-8L65 377.3c5.4-5.6 7.9-13.5 6.5-21.2V142.3z\"><\/path><\/svg> &nbsp; \/ <a href=\"http:\/\/#\" target=\"_blank\">example<\/a><\/p><p><br><\/p><\/div><\/div>"
+        },
+        "footer": {
+            "content": "&copy;2019 Your website"
+        },
+        "_heading": {
+            "content": "EXAMPLE"
+        },
+        "_subtitle": {
+            "content": "<span style=\"color: rgb(255, 255, 255); font-size: 16.8px;\">This is an awesome parallax<\/span><br style=\"color: rgb(255, 255, 255); font-size: 16.8px;\"><span style=\"color: rgb(255, 255, 255); font-size: 16.8px;\">theme for WonderCMS!<\/span>"
+        },
+        "example_heading": {
+            "content": "NICE!"
+        },
+        "example_subtitle": {
+            "content": "<p>Example page!<br>\nThis is an awesome&nbsp;theme!<\/p>"
+        },
+        "favicon-ico_heading": {
+            "content": "favicon ico"
+        },
+        "favicon-ico_subtitle": {
+            "content": ""
+        },
+        "home_heading": {
+            "content": "EXAMPLE"
+        },
+        "home_subtitle": {
+            "content": "<p>Welcome to this parallax theme!<br>\nThis is an simple example<\/p><p>on how to use this theme.<\/p>"
+        },
+        "404_heading": {
+            "content": "404"
+        },
+        "404_subtitle": {
+            "content": "<p>Oups, that page could<\/p><p>not be found.<\/p>"
+        },
+        "login_heading": {
+            "content": "Login"
+        },
+        "login_subtitle": {
+            "content": ""
+        }
+    }
+}
