a50d35cdd5ce12c2a66a15fa2acecd54d6ccd075
Robert Isoski <r@robiso.com>
2019-11-03 21:31:41 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,39 +1,50 @@
 /*
- *  WonderCMS 2.* Parallax Theme
+ *  WonderCMS 3.0.0 Parallax Theme
  *  by Stephan Stanisic
  *
  *  I'm not much of a front end guy so this is an attempt
  *  of making a theme. Most of this code is from codepen.io!
  *
+ *  All of this code is non critical, so it is okay if any of
+ *  it failes in older types of browsers.
+ *
  *  Credit where due:
  *  Parallax effect from P. Rachel, https://codepen.io/Prachl/pen/jjKzEy
  *  Showing and hiding of navbar from w3scools, https://www.w3schools.com/w3css/tryw3css_templates_parallax.htm
- *  Random generic smooth scroll jQuery code propably from csstricks
+ *  Random generic smooth scroll jQuery code probably from css tricks
  */
 
-// Parallax effect
+// Parallax effect (with config)
 window.addEventListener('scroll', function(e){
-  var scrolled = window.pageYOffset,
-      parallax = document.querySelector(".parallax"),
-      title = document.querySelector(".parallax .inner"),
-      coords = (scrolled * 0.4) + 'px',
-      offset = (scrolled * 0.1) + 'px';
-  parallax.style.transform = 'translateY(' + coords + ')';
-  title.style.transform = 'translateY(' + offset + ')';
+    var scrolled = window.pageYOffset,
+        parallax = document.querySelector(".parallax"),
+        title = document.querySelector(".parallax .inner"),
+        coords = (scrolled * 0.4) + 'px',
+        offset = (scrolled * (type == 'inverse' ? 0.4 : (type == 'parallax' ? -0.4 : 0.1))) + 'px';
+
+    if(['dual', 'parallax'].indexOf(type) >= 0)
+        parallax.style.transform = 'translateY(' + coords + ')';
+
+    if(['dual', 'parallax', 'inverse'].indexOf(type) >= 0)
+        title.style.transform = 'translateY(' + offset + ')';
 });
 
 // Change style of navbar on scroll
 window.addEventListener('scroll', function() {
-    var normal = "navbar navbar-default ", classes = "sticky";
+    if(Number(height.replace(/[^0-9]/g,'')) == 0 && loggedIn) return;
+    var normal = "navbar navbar-default", classes = " sticky";
 
     var navbar = document.querySelector("nav");
-    if (document.body.scrollTop > window.innerHeight * Number(height.replace(/[^0-9]/g,'')) / 100 - 100 || document.documentElement.scrollTop > window.innerHeight * Number(height.replace(/[^0-9]/g,'')) / 100 - 100) {
+    if(document.body.scrollTop > window.innerHeight * Number(height.replace(/[^0-9]/g,'')) / 100 - 100
+    || document.documentElement.scrollTop > window.innerHeight * Number(height.replace(/[^0-9]/g,'')) / 100 - 100) {
         navbar.className = normal + classes;
     } else {
         navbar.className = normal;
     }
 });
 
+// Make anchor links scroll instead of snap.
+// (It is jQuery, I know)
 $('a[href*="#"]').not('[href="#"]').not('[href="#0"]')
 .click(function(event) {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
@@ -60,30 +71,3 @@ $('a[href*="#"]').not('[href="#"]').not('[href="#0"]')
 
     }
 });
-
-
-/* This is my own 'awesome' code
-
-if(document.getElementById("adminPanel")) {
-
-    var adminPanel = document.getElementById("adminPanel");
-
-    var nav = adminPanel.getElementsByClassName("nav")[0];
-    var newNav = document.createElement("li");
-    newNav.innerHTML = '<a href="#themeOptions" aria-controls="themeOptions" role="tab" data-toggle="tab">Theme options</a>';
-    nav.appendChild(newNav);
-
-    var tabs = adminPanel.getElementsByClassName("tab-content")[0];
-    var newTab = document.createElement("div");
-    newTab.className = "tab-pane";
-    newTab.id = "themeOptions";
-    newTab.innerHTML = `
-        <p class="subTitle">Background image</p>
-        <div class="change"><p>Upload a image with the name '<b>${page}.jpg</b>' to the files section.</p></div>
-
-        <p class="subTitle">Header height</p>
-        <div class="form-group"><div class="change"><select class="form-control" name="themeSelect" onchange="fieldSave('themeHeaderHeight',this.value,'pages');"><option value="${height}">Current: ${height.replace(/[^0-9]/g,'')}%</option><option value="100vh">100% (default)</option><option value="90vh">90%</option><option value="80vh">80%</option><option value="70vh">70%</option><option value="60vh">60%</option></select></div></div>
-    `;
-    tabs.appendChild(newTab);
-
-} */
--- theme.php
+++ theme.php
@@ -1,112 +1,60 @@
 <?php
 
-$current_page = wCMS::slugify(wCMS::page('title'));
+global $Wcms;
 
-function alterAdmin($args) {
-    if(!wCMS::$loggedIn) return $args;
+$page_image = $Wcms->asset('images/default.jpg');
+$heading = $Wcms->page('title');
 
-    $doc = new DOMDocument();
-    @$doc->loadHTML($args[0]);
+$height = 100;
+$subtitle = "";
+$type = "";
+$readMore = "";
 
-    $label = $doc->createElement("p");
-    $label->setAttribute("class", "subTitle");
-    $label->nodeValue = "Header height (in %)";
+if($Wcms->currentPage == $Wcms->get('config', 'login')) {
+	$subtitle = $Wcms->page('content');
+} else {
+    // Check if image for this page exisis
+    if(isset($Wcms->get("pages", $Wcms->currentPage)->background) && $Wcms->get("pages", $Wcms->currentPage)->background != "") {
+        $page_image = "data/files/" . $Wcms->get("pages", $Wcms->currentPage)->background;
+    }
 
-    $doc->getElementById("currentPage")->insertBefore($label, $doc->getElementById("currentPage")->lastChild);
+    $height = isset($Wcms->get('pages', $Wcms->currentPage)->themeHeaderHeight) ? $Wcms->get('pages', $Wcms->currentPage)->themeHeaderHeight : 100;
 
-    $wrapper = $doc->createElement("div");
-    $wrapper->setAttribute("class", "change");
+    $heading = getEditableArea("heading", $Wcms->page('title'));
+    $subtitle = getEditableArea("subtitle", "");
 
-    $input = $doc->createElement("div");
-    $input->setAttribute("class", "editText");
-    $input->setAttribute("data-target", "pages");
-    $input->setAttribute("id", "themeHeaderHeight");
-    $input->nodeValue = isset(wCMS::get('pages', wCMS::slugify(wCMS::page('title')))->themeHeaderHeight) ? wCMS::get('pages', wCMS::slugify(wCMS::page('title')))->themeHeaderHeight : "100vh";
-
-    $wrapper->appendChild($input);
-
-    $doc->getElementById("currentPage")->insertBefore($wrapper, $doc->getElementById("currentPage")->lastChild);
-
-    $args[0] = preg_replace('~<(?:!DOCTYPE|/?(?:html|body))[^>]*>\s*~i', '', $doc->saveHTML());
-    return $args;
+    $type = isset($Wcms->get('pages', $Wcms->currentPage)->parallax) ? $Wcms->get('pages', $Wcms->currentPage)->parallax : "dual";
+    $readMore = isset($Wcms->get('pages', $Wcms->currentPage)->readMoreText) ? $Wcms->get('pages', $Wcms->currentPage)->readMoreText : "Read more";
 }
-wCMS::addListener('settings', 'alterAdmin');
-
-$page_image = wCMS::asset('images/default.jpg');
-
-// Check if image for this page exisis
-$files = glob("files/*");
-foreach($files as $file) {
-	$filename = pathinfo($file, PATHINFO_FILENAME);
-	if($filename == $current_page) {
-		$page_image = $file;
-		break;
-	}
-}
-
-
-function getEditableArea($name, $default) {
-	$block = wCMS::slugify(wCMS::page('title')) . "_" . $name;
-
-	// Check if the newEditableArea area is already exists, if not, create it
-	$value = null;
-	if (empty(wCMS::get('blocks',$block))) {
-		wCMS::set('blocks',$block, 'content', $default);
-	} else {
-		$value = wCMS::get('blocks',$block,'content');
-	}
-
-	if (wCMS::$loggedIn) {
-        if($value == null) return "<script>history.go(0)</script>";
-
-		return wCMS::block($block);
-	}
-
-	// If not logged in, return block in non-editable mode
-	return $value;
-}
-
-$height = isset(wCMS::get('pages', $current_page)->themeHeaderHeight) ? wCMS::get('pages', $current_page)->themeHeaderHeight : "100";
-
-$heading = getEditableArea("heading", wCMS::page('title'));
-$subtitle = getEditableArea("subtitle", wCMS::page('description'));
-
-if($current_page == 'login')
-	$subtitle = wCMS::page('content');
 
 ?>
+<!DOCTYPE html>
 <!--
-	WonderCMS 2.* Parallax Theme
+	WonderCMS 3.0.0 Parallax Theme
 	by Stephan Stanisic
 -->
-<!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title><?=wCMS::get('config','siteTitle')?> - <?=wCMS::page('title')?></title>
-	<meta name="description" content="<?=wCMS::page('description')?>">
-	<meta name="keywords" content="<?=wCMS::page('keywords')?>">
+	<title><?=$Wcms->get('config','siteTitle')?> - <?=$Wcms->page('title')?></title>
+	<meta name="description" content="<?=$Wcms->page('description')?>">
+	<meta name="keywords" content="<?=$Wcms->page('keywords')?>">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link rel="stylesheet" href="<?=wCMS::asset('css/style.css')?>">
+	<link rel="stylesheet" href="<?=$Wcms->asset('css/style.css')?>">
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400&display=swap" rel="stylesheet">
-	<?=wCMS::css()?>
+	<?=$Wcms->css()?>
+
 	<?php /* Translate php to some javascript variables and css rules. Please don't replicate this. */ ?>
-	<script>
-	var page = <?=json_encode($current_page)?>;
-	var heading = <?=json_encode($heading)?>;
-	var subtitle = <?=json_encode($subtitle)?>;
-	var image = <?=json_encode($page_image)?>;
-	var height = <?=json_encode($height)?>;
-	</script>
-	<style> .parallax { height: <?=$height?>vh; } </style>
+	<script>var page=<?=json_encode($Wcms->currentPage)?>,heading=<?=json_encode($heading)?>,subtitle=<?=json_encode($subtitle)?>,image=<?=json_encode($page_image)?>,height=<?=json_encode($height)?>,type=<?=json_encode($type)?>,loggedIn=<?=json_encode($Wcms->loggedIn)?>;</script>
+	<style>.parallax{height:<?=htmlentities($height)?>vh;}</style>
 </head>
 <body>
-	<?=wCMS::alerts()?>
-	<?=wCMS::settings()?>
+	<?=$Wcms->alerts()?>
+	<?=$Wcms->settings()?>
 
-	<nav class="navbar navbar-default">
+	<nav class="navbar navbar-default<?php if($height == 0)echo " background"; if(!$Wcms->loggedIn && $height == 0)echo " sticky no-animation"; ?>">
 		<div class="container">
 			<div class="navbar-header">
 				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -115,55 +63,72 @@ if($current_page == 'login')
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="<?=wCMS::url()?>">
-					<?=wCMS::get('config','siteTitle')?>
+				<a class="navbar-brand" href="<?=$Wcms->url()?>">
+					<?=$Wcms->get('config','siteTitle')?>
 				</a>
 			</div>
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       			<ul class="nav navbar-nav navbar-right">
-					<?=wCMS::menu()?>
+					<?=$Wcms->menu()?>
 				</ul>
 			</div>
 		</div>
 	</nav>
 
+    <?php if($height != 0): ?>
+
     <header class="parallax-wrapper">
         <div class="parallax" style='background-image: url(<?=json_encode($page_image)?>);'>
             <div class="inner">
                 <h1><?= $heading ?></h1>
-                <?= wCMS::$loggedIn ? $subtitle : "<p>$subtitle</p>" ?>
+                <?= $Wcms->loggedIn ? $subtitle : "<p>$subtitle</p>" ?>
             </div>
-			<a href="#content" class="scrolly">Read more<br>
+            <?php if($readMore): ?>
+			<a href="#content" class="scrolly"><?=$readMore?><br>
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M119.5 262.9L3.5 145.1c-4.7-4.7-4.7-12.3 0-17l7.1-7.1c4.7-4.7 12.3-4.7 17 0L128 223.3l100.4-102.2c4.7-4.7 12.3-4.7 17 0l7.1 7.1c4.7 4.7 4.7 12.3 0 17L136.5 263c-4.7 4.6-12.3 4.6-17-.1zm17 128l116-117.8c4.7-4.7 4.7-12.3 0-17l-7.1-7.1c-4.7-4.7-12.3-4.7-17 0L128 351.3 27.6 249.1c-4.7-4.7-12.3-4.7-17 0l-7.1 7.1c-4.7 4.7-4.7 12.3 0 17l116 117.8c4.7 4.6 12.3 4.6 17-.1z" fill="#ffffff"/></svg></a>
+            <?php endif; ?>
         </div>
     </header>
+
+    <?php endif; ?>
+
+    <?php if($Wcms->currentPage != $Wcms->get('config', 'login')): ?>
 
 	<div class="container" id="content">
 		<div class="row">
 			<div class="col-lg-12 text-center padding40">
-				<?=wCMS::page('content')?>
+				<?=$Wcms->page('content')?>
 			</div>
 		</div>
 	</div>
 
 	<div class="container-fluid CTA">
 			<div class="text-center padding40">
-				<?=wCMS::block('subside')?>
-
+				<?=$Wcms->block('subside')?>
 			</div>
 	</div>
+
+    <?php else: ?>
+
+    <style>.parallax .scrolly { display: none }</style>
+
+    <?php endif; ?>
 
 	<footer class="container-fluid">
 		<div class="text-center padding20">
 			<br><br>
-			<?=wCMS::footer()?>
+			<?=$Wcms->footer()?>
 			<br><br><br>
 		</div>
 	</footer>
 
 	<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha256-U5ZEeKfGNOja007MMD3YBI0A3OSZOQbeG6z2f2Y0hu8=" crossorigin="anonymous"></script>
-	<?=wCMS::js()?>
-    <script src="<?=wCMS::asset('js/script.js')?>"></script>
+	<?=$Wcms->js()?>
+    <script src="<?=$Wcms->asset('js/script.js')?>"></script>
+
+    <?php /* Add some overwrites for pages with different parallax types. */ ?>
+    <?php if($type == "scroll") echo "<style>.parallax{background-attachment:fixed;}</style>"; ?>
+    <?php if($height == 100) echo "<style>.parallax{padding-top:0;}</style>"; ?>
 </body>
 </html>
